86e45dfd90e09f105cce1e4185e270cbb4535657
dels1975 <dels1975@gmail.com>
2021-09-27 13:36:23 +0200

    creazione gerarchie

---
+++ Lezione_3/iterazioneOggetti.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const persona = {
+    nome: "Salvatore",
+    cognome: "De Luca",
+    eta: 45
+}
+
+// iterare le coppie nome:valore dell'oggetto
+
+// 1° metodo "for in": applicabile sugli oggetti
+for (let chiave in persona) {
+    // console.log(persona.chiave); con la dot notation non funziona!!
+    console.log(`${chiave}: ${persona[chiave]}`);
+}
+
+// 2° metodo "for of": applicabile sugli array
+for (let [c, v] of Object.entries(persona)) {
+    console.log(`${c}-> ${v}`)
+}
