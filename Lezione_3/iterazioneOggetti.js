37f8617a1cb5d8993cb36a02ef5a615523965716
GitGmail!1975 <dels1975@gmail.com>
2021-09-27 22:49:39 +0200

    Svolgimento soluzione 2: ordinamento dei voti tramite sort

--- Lezione_3/iterazioneOggetti.js
+++ Lezione_3/iterazioneOggetti.js
@@ -1,25 +1,25 @@
 "use strict";
 
 const persona = {
-    nome: "Salvatore",
-    cognome: "De Luca",
-    eta: 45
-}
+  nome: "Salvatore",
+  cognome: "De Luca",
+  eta: 45,
+};
 
 // iterare le coppie nome:valore dell'oggetto
 
 // 1° metodo "for in": applicabile sugli oggetti
 for (let chiave in persona) {
-    // console.log(persona.chiave); con la dot notation non funziona!!
-    console.log(`${chiave}: ${persona[chiave]}`);
+  // console.log(persona.chiave); con la dot notation non funziona!!
+  console.log(`${chiave}: ${persona[chiave]}`);
 }
 
 // 2° metodo "for of": applicabile sugli array
 for (let [c, v] of Object.entries(persona)) {
-    console.log(`${c}-> ${v}`)
+  console.log(`${c}-> ${v}`);
 }
 
 // 2° metodo "for of": applicabile sugli array / con Object.Keys si iterano solo le chiavi
-for ( let c of Object.keys(persona)) {
-    console.log(c)
+for (let c of Object.keys(persona)) {
+  console.log(c);
 }
