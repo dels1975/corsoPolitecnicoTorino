101c529f186b7f82a7f54319b9c1ae9b36241810
GitGmail!1975 <dels1975@gmail.com>
2021-10-31 10:55:40 +0100

    exercize aggiornato con codewars

---
+++ exercize.js
@@ -0,0 +1,34 @@
+"use strict";
+
+//  -- EXERCIZE:  dato un intero, trova tutti i divisori per cui il numero è divisibile, tranne 1 e se stesso e verifica se il numero è un numero primo
+function divisors(integer) {
+  let arrDiv = [];
+  for (let i = 2; i < integer; i++) {
+    if (integer % i === 0) {
+      arrDiv.push(i);
+    }
+  }
+  // verifica se l'array ha una lunghezza > 0
+  return arrDiv.length ? arrDiv : `${integer} is prime`;
+}
+console.log(divisors(1721453));
+
+// -- EXERCIZE:  Controlla se una stringa ha la stessa quantità di "x" e "o"--
+
+// Il metodo deve restituire un valore booleano e non fa distinzione tra maiuscole e minuscole.
+// La stringa può contenere qualsiasi carattere.
+
+// Il metodo replace() restituisce una nuova stringa con alcune o tutte le corrispondenze di un modello sostituite da una sostituzione.
+// Il modello può essere una stringa o una RegExp e la sostituzione può essere una stringa o una funzione da chiamare per ogni corrispondenza.
+// Se pattern è una stringa, verrà sostituita solo la prima occorrenza.
+
+// g - corrispondenze globali. Ad esempio, /ab/g indica la corrispondenza di tutte le occorrenze del testo 'ab' mentre
+// /ab/ indica la corrispondenza della prima occorrenza del testo 'ab'.
+// i - corrispondenze senza distinzione tra maiuscole e minuscole.
+// Ad esempio, /aB/i significa corrispondenza con 'ab', 'aB', 'Ab' e 'AB'.
+
+function XO(str) {
+  return str.replace(/o/gi, "").length == str.replace(/x/gi, "").length;
+}
+
+console.log(XO("iol moXxndxo"));
--- myExercise/classifica.js
+++ myExercise/classifica.js
@@ -1,52 +1,68 @@
-"use strict"
+"use strict";
 
 function Squadra(name, pt, pg, v, n, p) {
-    this.name = name;
-    this.pt = pt;
-    this.pg = pg;
-    this.v = v;
-    this.n = n;
-    this.p = p;
+  this.name = name;
+  this.pt = pt;
+  this.pg = pg;
+  this.v = v;
+  this.n = n;
+  this.p = p;
 }
 
 function Classifica() {
-    this.list =  [];
+  this.list = [];
 
-    this.init = () => {
-        this.list.push(
-            new Squadra('Napoli', 24, 8, 8, 0, 0 ),
-            new Squadra('Milan', 22, 8, 7, 1, 0 ),
-            new Squadra('Inter', 17, 8, 5, 2, 1 )
-        )
-    } 
+  this.init = () => {
+    this.list.push(
+      new Squadra("Napoli", 24, 8, 8, 0, 0),
+      new Squadra("Milan", 22, 8, 7, 1, 0),
+      new Squadra("Inter", 17, 8, 5, 2, 1)
+    );
+  };
 
-    this.getAll = () => {
-        return this.list;
-    }
+  this.getAll = () => {
+    return this.list;
+  };
+
+  this.getFirst = () => {
+    return Math.max.apply(
+      null,
+      this.list.map((firstPos) => firstPos.pt)
+    );
+  };
 }
 
-/* 
-<tr>
-          <td>Milan</td>
-          <td>24</td>
-          <td>8</td>
-          <td>8</td>
-          <td>0</td>
-          <td>0</td>
-        </tr>
-*/
-const squadra = new Squadra();
-const classificaSquadra = new Classifica();
-/* function createClassificaRiga(squadra) {
-    return `<tr>
-          <td>${squadra.name}</td>
-          <td>${squadra.pt}</td>
-          <td>${squadra.pg}</td>
-          <td>${squadra.v}</td>
-          <td>${squadra.n}</td>
-          <td>${squadra.p}</td>
-        </tr>`
-} */
+//RIEMPIRE LA TABELLA CON LE SQUADRE - DOM MANIPULATION
+function writeRow(el) {
+  return `<tr>
+    <td>${el.name}</td>
+    <td>${el.pt}</td>
+    <td>${el.pg}</td>
+    <td>${el.v}</td>
+    <td>${el.n}</td>
+    <td>${el.p}</td>
+    </tr>`;
+}
 
-console.log(classificaSquadra.init());
-console.log(classificaSquadra.getAll());
+function fillClassificaTable(squadre) {
+  const classificaTable = document.getElementById("squadra");
+  for (const el of squadre) {
+    // creare la riga
+    const elSquadra = writeRow(el);
+    // aggiungere la riga
+    classificaTable.insertAdjacentHTML("afterbegin", elSquadra);
+  }
+}
+
+// console.log(squadre);
+
+// MAIN
+const classificaSerieA = new Classifica();
+classificaSerieA.init();
+const squadre = classificaSerieA.getAll();
+fillClassificaTable(squadre);
+
+const primaClassifica = classificaSerieA.getFirst();
+
+console.log(squadre);
+console.log(primaClassifica);
