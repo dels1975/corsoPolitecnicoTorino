4631aa9e468eeca1e485e8ef2e326c8a7a9a82c4
GitGmail!1975 <dels1975@gmail.com>
2022-06-29 19:49:49 +0200

    add react-scores

---
+++ provaReact/prova3/src/API.js
@@ -0,0 +1,131 @@
+/**
+ * All the API calls
+ */
+import Course from './models/Course';
+import Exam from './models/Exam';
+
+const BASEURL = '/api';
+
+async function getAllCourses() {
+  // call: GET /api/courses
+  const response = await fetch(BASEURL + '/courses');
+  const coursesJson = await response.json();
+  if (response.ok) {
+    return coursesJson.map((co) => Course.from(co));
+  } else {
+    throw coursesJson;  // an object with the error coming from the server
+  }
+}
+
+async function getAllExams() {
+  // call: GET /api/exams
+  const response = await fetch(BASEURL + '/exams');
+  const examsJson = await response.json();
+  if (response.ok) {
+    return examsJson.map((ex) => Exam.from(ex));
+  } else {
+    throw examsJson;  // an object with the error coming from the server
+  }
+}
+
+function deleteExam(coursecode) {
+  // call: DELETE /api/exams/:coursecode
+  return new Promise((resolve, reject) => {
+    fetch(BASEURL + '/exams/' + coursecode, {
+      method: 'DELETE',
+    }).then((response) => {
+      if (response.ok) {
+        resolve(null);
+      } else {
+        // analyze the cause of error
+        response.json()
+          .then((message) => { reject(message); }) // error message in the response body
+          .catch(() => { reject({ error: "Cannot parse server response." }) }); // something else
+        }
+    }).catch(() => { reject({ error: "Cannot communicate with the server." }) }); // connection errors
+  });
+}
+
+function addExam(exam) {
+  // call: POST /api/exams
+  return new Promise((resolve, reject) => {
+    fetch(BASEURL + '/exams', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({code: exam.coursecode, score: exam.score, date: exam.date}),
+      }).then((response) => {
+        if (response.ok) {
+          resolve(null);
+        } else {
+          // analyze the cause of error
+          response.json()
+            .then((message) => { reject(message); }) // error message in the response body
+            .catch(() => { reject({ error: "Cannot parse server response." }) }); // something else
+        }
+    }).catch(() => { reject({ error: "Cannot communicate with the server." }) }); // connection errors
+  });
+}
+
+function updateExam(exam) {
+  // call: PUT /api/exams/:coursecode
+  return new Promise((resolve, reject) => {
+    fetch(BASEURL + '/exams/' + exam.coursecode, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+        body: JSON.stringify({code: exam.coursecode, score: exam.score, date: exam.date}),
+    }).then((response) => {
+      if (response.ok) {
+        resolve(null);
+      } else {
+        // analyze the cause of error
+        response.json()
+          .then((obj) => { reject(obj); }) // error message in the response body
+          .catch(() => { reject({ error: "Cannot parse server response." }) }); // something else
+      }
+    }).catch(() => { reject({ error: "Cannot communicate with the server." }) }); // connection errors
+  });
+}
+
+async function logIn(credentials) {
+  let response = await fetch('/api/sessions', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(credentials),
+  });
+  if(response.ok) {
+    const user = await response.json();
+    return user.name;
+  }
+  else {
+    try {
+      const errDetail = await response.json();
+      throw errDetail.message;
+    }
+    catch(err) {
+      throw err;
+    }
+  }
+}
+
+async function logOut() {
+  await fetch('/api/sessions/current', { method: 'DELETE' });
+}
+
+async function getUserInfo() {
+  const response = await fetch(BASEURL + '/sessions/current');
+  const userInfo = await response.json();
+  if (response.ok) {
+    return userInfo;
+  } else {
+    throw userInfo;  // an object with the error coming from the server
+  }
+}
+
+const API = {getAllCourses, getAllExams, deleteExam, addExam, updateExam, logIn, logOut, getUserInfo};
+export default API;
